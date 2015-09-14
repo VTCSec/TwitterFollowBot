84fd7a91bf2daf8dd8c271cb0cbab64eb6a436f0
Conor <conorpp@vt.edu>
2015-09-14 17:04:53 -0400

    that's wat u call persistence

--- mr_vtcsec.py
+++ mr_vtcsec.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
 from TwitterFollowBot import TwitterBot
-import time
+import time,sys
 
-bot = TwitterBot('config.txt')
+bot = TwitterBot(sys.argv[1])
 
 while True:
     print 'retweeting..'
-    bot.auto_rt_user_popular('_conorpp',count=3)
+    try:
+        bot.auto_rt_user_popular('_conorpp',count=3)
+    except Exception as e:
+        print(e)
     time.sleep(5)
