cccdfaf6ce884e2096c4b2e4de467f8953089aa5
Conor <conorpp@vt.edu>
2015-10-15 12:10:23 -0400

    added attachments

--- mr_vtcsec.py
+++ mr_vtcsec.py
@@ -20,14 +20,36 @@ def build_fb_post(t):
 
     author = re.search(r'@(.+?):',t['text']).groups()[0]
     src = build_link(t,author)
+    pic = None
     if 'entities' in t:
         if len(t['entities']['urls']):
             src = t['entities']['urls'][0]['expanded_url']
-    print author
-    return """
-        %s (@%s, %s)
-    """ % (text,author,src)
     
+        if 'media' in t['entities'] and len(t['entities']['media']):
+
+            med = t['entities']['media'][0]
+
+            if 'media_url' in med:
+                pic = med['media_url']
+
+            if 'media_url_https' in t['entities']:
+                pic = med['media_url_https']
+
+
+    print author
+    attachment = None
+    if pic:
+        attachment =  {
+                 'name': 'vtcsec bot',
+                 'link': src,
+                 'caption': 'vtcsec bot likes this',
+                 'description': text,
+                 'picture': pic
+                 }
+
+    return ("""
+        %s (@%s, %s)
+    """ % (text,author,src), attachment)
 
 while True:
     print 'retweeting..'
@@ -37,11 +59,17 @@ while True:
         #tweets = [{'text':'RT @_conorpp: rab fdewfhewo http://example.com'}]
         for i in tweets:
             text = i['retweeted_status']['text']
-            if '@' in text:
-                continue
-            post = build_fb_post(i)
+            #if '@' in text:
+            #    continue
+            tbot.print_tweet(i)
+            post, pic = build_fb_post(i)
             print('posted to facebook: ', post)
-            fbot.api.put_wall_post(post)
+
+            if pic: 
+                fbot.api.put_wall_post(post, attachment=pic)
+                print ('posted media', pic['picture'])
+            else: fbot.api.put_wall_post(post)
+
     except Exception as e:
         print(e)
     # only run once per 3 minutes to avoid rate limit
